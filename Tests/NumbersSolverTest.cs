f87ba1e102bad369bb113711fcd76bd220830de0
Max Miller <me@maxcmiller.com>
2021-12-26 14:34:04 +1000

    Use platform-independent newlines

--- Tests/NumbersSolverTest.cs
+++ Tests/NumbersSolverTest.cs
@@ -23,8 +23,8 @@ public class Tests
         int[] numbers = {10, 5, 1, 2, 3, 4};
         string input = GenerateInput(target, numbers);
         string expected = GenerateOutput(target, numbers)
-                          + "Solved after 273,009 attempts.\r\n"
-                          + "(10 × (5 × ((1 + 4) × 2)))\r\n";
+                          + "Solved after 273,009 attempts." + Environment.NewLine
+                          + "(10 × (5 × ((1 + 4) × 2)))" + Environment.NewLine;
 
         AssertSolvesTo(input, expected);
     }
@@ -36,8 +36,8 @@ public class Tests
         int[] numbers = {50, 100, 3, 1, 1, 8};
         string input = GenerateInput(target, numbers);
         string expected = GenerateOutput(target, numbers)
-                          + "Solved after 2,842,327 attempts.\r\n"
-                          + "(((50 + 100) × (3 + 1)) + (1 - 8))\r\n";
+                          + "Solved after 2,842,327 attempts." + Environment.NewLine
+                          + "(((50 + 100) × (3 + 1)) + (1 - 8))" + Environment.NewLine;
         
         AssertSolvesTo(input, expected);
     }
@@ -49,11 +49,11 @@ public class Tests
         int[] numbers = {25, 50, 3, 1, 6, 7};
         string input = GenerateInput(target, numbers, NumbersSolver.SolveMode.All);
         string expected = GenerateOutput(target, numbers)
-                          + "Found solution: (((50 × 7) + 6) × (3 - 1))\r\n"
-                          + "Found solution: ((((25 × (50 + 7)) - 1) × 3) ÷ 6)\r\n"
-                          // + "Found solution: (((25 × (50 + 7)) - 1) ÷ (6 ÷ 3))\r\n" // TODO should this be considered a distinct solution?
-                          + "Found solution: ((25 × 3) - ((1 - 50) × (6 + 7)))\r\n"
-                          + "Found 3 solutions in 33,802,560 attempts\r\n";
+                          + "Found solution: (((50 × 7) + 6) × (3 - 1))" + Environment.NewLine
+                          + "Found solution: ((((25 × (50 + 7)) - 1) × 3) ÷ 6)" + Environment.NewLine
+                          // + "Found solution: (((25 × (50 + 7)) - 1) ÷ (6 ÷ 3))" + Environment.NewLine // TODO should this be considered a distinct solution?
+                          + "Found solution: ((25 × 3) - ((1 - 50) × (6 + 7)))" + Environment.NewLine
+                          + "Found 3 solutions in 33,802,560 attempts" + Environment.NewLine;
 
         AssertSolvesTo(input, expected);
     }
@@ -66,8 +66,8 @@ public class Tests
         int[] numbers = {25, 100, 6, 10, 4, 5};
         string input = GenerateInput(target, numbers, NumbersSolver.SolveMode.All);
         string expected = GenerateOutput(target, numbers)
-                          + "Found solution: (25 + (((100 × (6 × 4)) + 10) ÷ 5))\r\n"
-                          + "Found 1 solution in 33,802,560 attempts\r\n";
+                          + "Found solution: (25 + (((100 × (6 × 4)) + 10) ÷ 5))" + Environment.NewLine
+                          + "Found 1 solution in 33,802,560 attempts" + Environment.NewLine;
 
         AssertSolvesTo(input, expected);
     }
@@ -79,22 +79,22 @@ public class Tests
         int[] numbers = {50, 100, 2, 7, 4, 3};
         string input = GenerateInput(target, numbers, NumbersSolver.SolveMode.MostIntuitive);
         string expected = GenerateOutput(target, numbers)
-                          + "Found solution: (((100 + 3) × 7) - 2) [intuition score: 120]\r\n"
-                          + "Found solution: (50 + (((100 - 4) × 7) - 3)) [intuition score: 160]\r\n"
-                          + "Found solution: (50 - ((7 × (4 - 100)) + 3)) [intuition score: 160]\r\n"
-                          + "Found solution: (((100 + 3) × 7) + (2 - 4)) [intuition score: 150]\r\n"
-                          + "Found solution: (((100 + 3) × 7) - (4 ÷ 2)) [intuition score: 180]\r\n"
-                          + "Found solution: (50 + (((100 - (2 + 3)) × 7) + 4)) [intuition score: 180]\r\n"
-                          + "Found solution: (((((50 × 100) + 2) + 3) ÷ 7) + 4) [intuition score: 200]\r\n"
-                          + "Found solution: (((50 + (100 - 7)) × (2 + 3)) + 4) [intuition score: 180]\r\n"
-                          + "Found solution: (((50 + ((100 - 4) × 2)) × 3) - 7) [intuition score: 200]\r\n"
-                          + "Found solution: ((50 - (((2 - 100) + 3) × 7)) + 4) [intuition score: 190]\r\n"
-                          + "Found solution: (((50 - (2 × (4 - 100))) × 3) - 7) [intuition score: 210]\r\n"
-                          + "Found solution: ((((50 × 7) - 3) × 2) + (100 ÷ 4)) [intuition score: 220]\r\n"
-                          + "Found solution: (((((50 × 4) - 100) + 3) × 7) - 2) [intuition score: 200]\r\n"
-                          + "Found solution: ((((100 × 2) - 7) × 4) - (50 + 3)) [intuition score: 200]\r\n"
-                          + "Found solution: ((100 ÷ 4) - (2 × (3 - (50 × 7)))) [intuition score: 230]\r\n"
-                          + "Most intuitive solution: (((100 + 3) × 7) - 2)\r\n";
+                          + "Found solution: (((100 + 3) × 7) - 2) [intuition score: 120]" + Environment.NewLine
+                          + "Found solution: (50 + (((100 - 4) × 7) - 3)) [intuition score: 160]" + Environment.NewLine
+                          + "Found solution: (50 - ((7 × (4 - 100)) + 3)) [intuition score: 160]" + Environment.NewLine
+                          + "Found solution: (((100 + 3) × 7) + (2 - 4)) [intuition score: 150]" + Environment.NewLine
+                          + "Found solution: (((100 + 3) × 7) - (4 ÷ 2)) [intuition score: 180]" + Environment.NewLine
+                          + "Found solution: (50 + (((100 - (2 + 3)) × 7) + 4)) [intuition score: 180]" + Environment.NewLine
+                          + "Found solution: (((((50 × 100) + 2) + 3) ÷ 7) + 4) [intuition score: 200]" + Environment.NewLine
+                          + "Found solution: (((50 + (100 - 7)) × (2 + 3)) + 4) [intuition score: 180]" + Environment.NewLine
+                          + "Found solution: (((50 + ((100 - 4) × 2)) × 3) - 7) [intuition score: 200]" + Environment.NewLine
+                          + "Found solution: ((50 - (((2 - 100) + 3) × 7)) + 4) [intuition score: 190]" + Environment.NewLine
+                          + "Found solution: (((50 - (2 × (4 - 100))) × 3) - 7) [intuition score: 210]" + Environment.NewLine
+                          + "Found solution: ((((50 × 7) - 3) × 2) + (100 ÷ 4)) [intuition score: 220]" + Environment.NewLine
+                          + "Found solution: (((((50 × 4) - 100) + 3) × 7) - 2) [intuition score: 200]" + Environment.NewLine
+                          + "Found solution: ((((100 × 2) - 7) × 4) - (50 + 3)) [intuition score: 200]" + Environment.NewLine
+                          + "Found solution: ((100 ÷ 4) - (2 × (3 - (50 × 7)))) [intuition score: 230]" + Environment.NewLine
+                          + "Most intuitive solution: (((100 + 3) × 7) - 2)" + Environment.NewLine;
 
         AssertSolvesTo(input, expected);
     }
@@ -106,7 +106,7 @@ public class Tests
         int[] numbers = {3, 7, 6, 2, 1, 7};
         string input = GenerateInput(target, numbers);
         string expected = GenerateOutput(target, numbers)
-                          + "No solution after 33,802,560 attempts\r\n";
+                          + "No solution after 33,802,560 attempts" + Environment.NewLine;
         
         AssertSolvesTo(input, expected);
     }
@@ -140,14 +140,14 @@ public class Tests
     private string GenerateOutput(int target, int[] numbers)
     {
         return "Enter target number (0-999): " +
-               $"Target is {target}.\r\n" +
+               $"Target is {target}." + Environment.NewLine +
                "Enter chosen number #1: " +
                "Enter chosen number #2: " +
                "Enter chosen number #3: " +
                "Enter chosen number #4: " +
                "Enter chosen number #5: " +
                "Enter chosen number #6: " +
-               $"Numbers are {String.Join(", ", numbers)}.\r\n" +
+               $"Numbers are {String.Join(", ", numbers)}." + Environment.NewLine +
                "Solution mode: first/all/intuitive (f/a/i): ";
     }
 }
